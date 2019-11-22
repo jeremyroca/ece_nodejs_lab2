2a5898e2b5e532beddf9cda24a3f8ae13114c036
jeremyroca <jeremyrocaroca@gmail.com>
2019-11-22 10:44:35 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+express = require('express')
+app = express()
+path = require('path')
+metrics = require('./metrics')
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('port', 1337)
+app.set('views', __dirname + "/view")
+app.set('view engine', 'ejs');
+
+app.get(
+    '/hello/:name', 
+    (req, res) => res.render('hello.ejs', {name: req.params.name})
+  )
+
+  app.get('/metrics.json', (req, res) => {
+    metrics.get((err, data) => {
+      if(err) throw err
+      res.status(200).json(data)
+    })
+  })
+
+
+app.listen(
+  app.get('port'), 
+  () => console.log(`server listening on ${app.get('port')}`)
+)
